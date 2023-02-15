63479fe235b1ddf661edee9b09c1e8c4a6209f0e
wave49192 <sirawich.tu@ku.th>
2023-02-15 12:07:48 +0700

    first commit

---
+++ src/main/java/ku/eatate/EatateApplication.java
@@ -0,0 +1,13 @@
+package ku.eatate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ku/eatate/EatateApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.eatate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
